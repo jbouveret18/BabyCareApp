1b32cbe49cde25a0ef6766c4770ddcad62cbd0e7
jbouveret18 <jbouveret18@gmail.com>
2023-04-22 16:36:09 +0200

    Fixed errors

--- app/src/main/java/com/example/babycare/MainActivity.java
+++ app/src/main/java/com/example/babycare/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.babycare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.SignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.sign_up);
+            }
+        });
     }
 }
