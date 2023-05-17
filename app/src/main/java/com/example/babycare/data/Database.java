e24b7d6a580b940326f88ba5b55112cf782eab51
jbouveret18 <you@example.com>
2023-05-17 12:02:20 +0200

    Database adress write function

--- app/src/main/java/com/example/babycare/data/Database.java
+++ app/src/main/java/com/example/babycare/data/Database.java
@@ -1,7 +1,11 @@
 package com.example.babycare.data;
 
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.babycare.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,5 +23,12 @@ public class Database extends AppCompatActivity {
         Address address = new Address(user.key, user.firstName, user.lastName, user.email, user.birthday,region,country,street,postalCode);
         databaseReference.child("address").child(String.valueOf(user.key)).setValue(address);
     }
-
+    EditText emailInput = findViewById(R.id.emailInput);
+    EditText lastNameInput = findViewById(R.id.lastNameInput);
+    EditText firstNameInput = findViewById(R.id.firstNameInput);
+    EditText birthdayInput = findViewById(R.id.birthdayInput);
+    AutoCompleteTextView countryInput = findViewById(R.id.countries_list);
+    AutoCompleteTextView regionInput = findViewById(R.id.region_list);
+    EditText streetInput = findViewById(R.id.streetInput);
+    EditText postalCodeInput = findViewById(R.id.postalCodeInput);
 }
