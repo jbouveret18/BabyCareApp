bd50568a4149c82a43f813c53bb51f02605656ea
jbouveret18 <you@example.com>
2023-05-17 10:42:33 +0200

    Lamda's added

--- app/src/main/java/com/example/babycare/MainActivity.java
+++ app/src/main/java/com/example/babycare/MainActivity.java
@@ -1,14 +1,13 @@
 
 package com.example.babycare;
 
-import com.example.babycare.sing_up.SignInActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import com.example.babycare.sing_up.SignInActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,18 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.SignUp);
         Button googleSignInButton = findViewById(R.id.Google);
-        googleSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchControllerActivity();
-            }
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.sign_up);
-            }
-        });
+        googleSignInButton.setOnClickListener(v -> launchControllerActivity());
+        button.setOnClickListener(v -> setContentView(R.layout.sign_up));
     }
     private void launchControllerActivity() {
         Intent intent = new Intent(this, SignInActivity.class);
