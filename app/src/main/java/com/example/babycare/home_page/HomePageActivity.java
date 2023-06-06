e407caf15fadc96250358836f4cb7fe922e4cec0
jbouveret18 <jbouveret18@gmail.com>
2023-06-06 20:58:57 +0200

    Added Adress oage activity

--- app/src/main/java/com/example/babycare/home_page/HomePageActivity.java
+++ app/src/main/java/com/example/babycare/home_page/HomePageActivity.java
@@ -17,9 +17,9 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
         openMap = findViewById(R.id.openMap);
         openMapIntent = new Intent(HomePageActivity.this, MapActivity.class);
-        setContentView(R.layout.home_page);
         openMap.setOnClickListener(view -> startActivity(openMapIntent));
     }
 }
